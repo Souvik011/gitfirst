6c62fe9d92be9775b9940c8528ac356362b69573
Souvik011 <iamsouvik011@gmail.com>
2022-10-19 22:39:33 +0530

    Task 4

--- index.js
+++ index.js
@@ -5,12 +5,23 @@
 //document.title = "Souvik First DOM ";
 //console.log(document.head);
 //console.log(document.links);
-var x = document.getElementById('header-title');
-console.log(x.innerText);
-console.log(x.textContent);
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
-var items = document.getElementsByClassName('title');
+//var x = document.getElementById('header-title');
+//console.log(x.innerText);
+//console.log(x.outerHTML)
+//var header = document.getElementById('main-header');
+//header.style.borderBottom = 'solid 3px #000';
+//var items = document.getElementsByClassName('title');
+//console.log(items[0]);
+//items[0].innerHTML='<b> Add Items </b>'
+//items[0].style.color="#00FF00";
+
+
+//Get Elements by Class
+
+var items = document.getElementsByClassName("list-group-item");
+console.log(items);
 console.log(items[0]);
-items[0].innerHTML='<b> Add Items </b>'
-items[0].style.color="#00FF00";
+items[1].textContent = 'Hello';
+items[1].style.fontWeight = 'Bold';
+items[1].style.backgroundColor = 'yellow';
+items[2].style.backgroundColor = 'green';
