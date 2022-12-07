6929842f4dca52033796d5dbf748ed531502a42f
Souvik011 <iamsouvik011@gmail.com>
2022-12-07 23:50:37 +0530

    functionality of edit Appointments

--- 13 new.js
+++ 13 new.js
@@ -14,12 +14,12 @@ function onsubmit(e){
     else{
         let Obj={
             name:namei.value,
-            Emailid:emaili.value,
-            Phoneno:numberi.value
+            email:emaili.value,
+            contactNumber:numberi.value
         }
-        axios.post("https://crudcrud.com/api/424091e9ef2649d9a4befa8c38a8b102/appoinmentData",Obj)
+        axios.post("https://crudcrud.com/api/0fd737a168104501ba123a946f24f4a4/appointmentData",Obj)
         .then((response)=>{
-            showonscreen(response.data)
+            showData(response.data)
 
             console.log(response)
         })
@@ -34,7 +34,7 @@ function onsubmit(e){
     }
 }
 window.addEventListener("DOMContentLoaded",()=>{
-    axios.get("https://crudcrud.com/api/424091e9ef2649d9a4befa8c38a8b102/appointmentData")
+    axios.get("https://crudcrud.com/api/0fd737a168104501ba123a946f24f4a4/appointmentData")
     .then((response)=>{
         console.log(response)
          for(var i=0;i<response.data.length;i++){
@@ -48,8 +48,8 @@ window.addEventListener("DOMContentLoaded",()=>{
 
 function showData(data){
     var li=document.createElement('li');
-    li.className='items';
-    li.appendChild(document.createTextNode(data.email));
+    li.className='item';
+    li.appendChild(document.createTextNode(`${data.name}- ${data.email}- ${data.contactNumber}`));
 
 
 
@@ -73,15 +73,15 @@ function showData(data){
             if(confirm('are you sure?')){
                 var li=e.target.parentElement;
                 // localStorage.removeItem(emaili.value);
-                axios.get(`https://crudcrud.com/api/4870f87315874cb184403d9dd48b778f/appoinmentData`)
+                axios.get(`https://crudcrud.com/api/0fd737a168104501ba123a946f24f4a4/appointmentData`)
                    .then((response)=>{
                        console.log(response)
                         for(var i=0;i<response.data.length;i++){
 
                             if(li.innerHTML.indexOf(response.data[i].email)!=-1){
-                            axios.delete(`https://crudcrud.com/api/4870f87315874cb184403d9dd48b778f/appoinmentData/${response.data[i]._id}`)
+                            axios.delete(`https://crudcrud.com/api/0fd737a168104501ba123a946f24f4a4/appointmentData/${response.data[i]._id}`)
                              .then()
-                             .catch(err=>console.log(err))
+                             .catch(err=>console.log(err));
                             }
                         }
                    })
@@ -98,26 +98,28 @@ function showData(data){
 
     function edititem(e){
         if(e.target==editbtn){
-        var li=e.target.parentElement;
-        for(var i=0;i<itemList.getElementsByClassName('items').length;i++)
-            {
-                if(itemList.getElementsByClassName('items')[i]==li){
-                    
-                    let myobj_deserialized1=JSON.parse(itemList.getElementsByClassName('items')[i].firstChild.textContent)
-                    
-                    myForm.querySelector('#name').value=myobj_deserialized1.name;
-                    myForm.querySelector('#email').value=myobj_deserialized1.email;
-                }
+            var li=e.target.parentElement;
+            axios.get(`https://crudcrud.com/api/0fd737a168104501ba123a946f24f4a4/appointmentData`)
+            .then((response)=>{
+            for(var i=0;i<response.data.length;i++){
+            if(li.innerHTML.indexOf(response.data[i].email)!=-1) {
+
+                myForm.querySelector('#name').value=response.data[i].name
+                myForm.querySelector('#email').value=response.data[i].email;
+                myForm.querySelector('#number').value=response.data[i].contactNumber;
+                axios.delete(`https://crudcrud.com/api/0fd737a168104501ba123a946f24f4a4/appointmentData/${response.data[i]._id}`)
+                .then()
+                .catch(err=>console.log(err));
+
             }
-        localStorage.removeItem(emaili.value);
-        itemList.removeChild(li);
         }
-    }
-    
-    
- 
-    
-        
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+    itemList.removeChild(li);
+}
+}    
     }
 
 
